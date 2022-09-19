2b457e12562d9c5bc10191b0d1b444412e86e217
Justin <kellerjustin@protonmail.com>
2022-09-19 16:29:22 -0500

    file for cash flow testing

---
+++ gnucash_business_reports/cash_flow.py
@@ -0,0 +1,32 @@
+from .builder import GnuCash_Data_Analysis
+
+
+gda = GnuCash_Data_Analysis()
+gda.year = 2021
+tx_w_depr = gda.get_farm_cash_transactions(include_depreciation=True)
+tx_w_depr.reset_index(inplace=True)
+tx_w_depr.sort_values(by=["account_code", "post_date"], inplace=True)
+
+tx_sum = tx_w_depr.groupby(
+    [
+        "account_type",
+    ]
+).sum()
+print(tx_sum)
+
+"""
+print(depr)
+print(tx)
+"""
+# balance_sheet = gda.get_balance_sheet()
+# balance_sheet = balance_sheet[balance_sheet["post_date"].dt.year <= year]
+# print(balance_sheet)
+# print(balance_sheet.groupby("balance_sheet_category").sum())
+
+# prices = gda.get_commodity_prices()
+# print(prices)
+
+# all_accounts = gda.get_all_accounts()
+# commodities = all_accounts["commodity_guid"].unique()
+# print(commodities.tolist())
+print(gda.get_balance_sheet())
