f23b336b8f725366b3c20a34453f1c7a5aaa677d
Justin <kellerjustin@protonmail.com>
2022-09-19 09:02:03 -0500

    for testing mostly

---
+++ gnucash_business_reports/bids.py
@@ -0,0 +1,39 @@
+from .builder import GnuCash_Data_Analysis
+
+gda = GnuCash_Data_Analysis()
+
+
+prices = gda.get_latest_commodity_bids()
+# print(prices.loc[prices.index.max()][["fullname", "cash"]])
+
+# prices = prices.groupby("fullname")["date", "cash"].max()
+
+print(prices)
+
+year = 2021
+balance_sheet = gda.get_balance_sheet()
+balance_sheet = balance_sheet[balance_sheet["post_date"].dt.year <= year]
+print(balance_sheet)
+balance_sheet_totals = balance_sheet.groupby("balance_sheet_category").sum()
+balance_sheet_totals.loc["Grain"] = (balance_sheet_totals.loc["Stock"]["qty"] * 2, 0, 0)
+
+"""
+total_row.extend(column_filler(latex_df))
+total_row.append(account_totals_dict[code])
+latex_df.loc["2021-12-31"] = total_row
+"""
+
+print(balance_sheet_totals)
+
+stock = gda.get_stock()
+print(stock.groupby("commodity_guid").sum())
+
+
+grain = (
+    gda.get_grain(2021)
+    .groupby("commodity_guid")
+    .sum()
+    .join(gda.get_latest_commodity_bids().set_index("commodity_guid"))
+)
+grain["value"] = grain["qty"] * grain["cash"]
+print(grain)
