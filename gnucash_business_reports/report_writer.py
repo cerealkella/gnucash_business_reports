f8b062d61082d1846b57bcc471e5c6ee779d062d
Justin <kellerjustin@protonmail.com>
2022-09-23 15:48:16 -0500

    removes indexing by date and hides int index

--- gnucash_business_reports/report_writer.py
+++ gnucash_business_reports/report_writer.py
@@ -1,9 +1,11 @@
 from .builder import GnuCash_Data_Analysis
 from .helpers import column_filler, column_type_changer
 from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
 
 gda = GnuCash_Data_Analysis()
-tx = gda.get_farm_cash_transactions()
+gda.year = 2021
+tx = gda.get_farm_cash_transactions(include_depreciation=True)
 
 
 account_codes = tx["account_code"].unique()
@@ -15,13 +17,15 @@ account_names_df = account_names_df["display"]
 account_names_df = account_names_df[~account_names_df.reset_index().duplicated().values]
 account_totals_series = account_totals["amt"].squeeze()
 account_totals_dict = account_totals_series.to_dict()
+account_names_dict = account_names_df.to_dict()
 
 latex = []
 for code in account_codes:
-    latex_df = tx[tx["account_code"] == code].set_index("post_date")
+    latex_df = tx[tx["account_code"] == code]  # .set_index("post_date")
     latex_df = latex_df[
         [
             "src_code",
+            "post_date",
             "description",
             "memo",
             "amt",
@@ -31,31 +35,37 @@ for code in account_codes:
     if len(latex_df.columns) > 2:
         total_row.extend(column_filler(latex_df))
     total_row.append(account_totals_dict[code])
-    latex_df.loc[f"{gda.year}-12-31"] = total_row
+    total_row[1] = datetime(gda.year, 12, 31)
+    latex_df.loc[len(latex_df)] = total_row
+    latex_df["post_date"] = latex_df["post_date"].astype("datetime64[ns]")
     latex_df.rename(
         columns={
             "src_code": "Src",
+            "post_date": "Date",
             "description": "Description",
             "memo": "Memo",
             "amt": "Amount",
         },
         inplace=True,
     )
-    latex_df.index.rename("Date", inplace=True)
+    # latex_df.style.format()
     latex_str = column_type_changer(
-        latex_df.style.format(
-            decimal=".", thousands=",", precision=2, escape="latex"
-        ).to_latex(
+        latex_df.style.hide(axis="index")
+        .format(
+            {"Date": lambda t: t.strftime("%Y-%m-%d")},
+            decimal=".",
+            thousands=",",
+            precision=2,
+            escape="latex",
+        )
+        .to_latex(
             hrules=False,
         ),
-        caption=code,
+        caption=account_names_dict[code],
     )
     latex.append(latex_str)
 
-
 latex_dict = dict(zip(account_codes, latex))
-account_names_dict = account_names_df.to_dict()
-
 
 file_loader = FileSystemLoader("templates")
 env = Environment(
@@ -79,5 +89,5 @@ template = env.get_template("tabularray.tex")
 output = template.render(
     data=report_details, data1=latex_dict, names=account_names_dict
 )
-with open("export/rendered.tex", "w") as f:
+with open(f"export/{gda.year}-rendered.tex", "w") as f:
     f.write(output)
