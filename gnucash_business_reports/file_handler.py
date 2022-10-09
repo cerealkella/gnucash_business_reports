f248124cc81fe7f5a8ec4235e4eee493ebb054e2
Justin <kellerjustin@protonmail.com>
2022-10-09 15:00:13 -0500

    adds directory monitoring logic

---
+++ gnucash_business_reports/file_handler.py
@@ -0,0 +1,43 @@
+import time
+from pathlib import Path
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from .config import get_config
+
+
+class MyHandler(FileSystemEventHandler):
+    def __init__(self):
+        config = get_config()["Elevator"]
+        self.match_len = len(config["file_match_pattern"])
+        if self.match_len > 0:
+            self.pattern_match = config["file_match_pattern"]
+        else:
+            self.pattern_match = None
+
+    def _event_handler(self, path):
+        downloaded_file = Path(path)
+        if (
+            downloaded_file.name[: self.match_len] == self.pattern_match
+            or self.pattern_match == None
+        ):
+            if downloaded_file.suffix == ".CSV":
+                print("we got us a match")
+
+    def on_created(self, event):
+        print(event.event_type + " -- " + event.src_path)
+        self._event_handler(event.src_path)
+
+
+def watcher(path=Path.joinpath(Path.home(), "Downloads")):
+    event_handler = MyHandler()
+    print(f"Monitoring directory: {path}")
+    observer = Observer()
+    observer.schedule(event_handler, path=path, recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
