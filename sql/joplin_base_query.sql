0eac9fc6a1830a9194ae3e431a28449567214470
Justin <kellerjustin@protonmail.com>
2024-01-13 08:07:28 -0600

    adds joplin base sql

---
+++ sql/joplin_base_query.sql
@@ -0,0 +1,10 @@
+select
+    id as joplin_id,
+    parent_id,
+    title,
+    body,
+    source,
+    source_application,
+    DATETIME(ROUND(user_updated_time / 1000), 'unixepoch', 'localtime') as created_date,
+    DATETIME(ROUND(user_created_time / 1000), 'unixepoch', 'localtime') as updated_date
+from notes
