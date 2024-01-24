2eddca4131bd5f4a7e31c8d959f2d8bd7f0b0084
Justin <kellerjustin@protonmail.com>
2024-01-24 10:26:05 -0600

    handling data types

--- sql/joplin_base_query.sql
+++ sql/joplin_base_query.sql
@@ -1,3 +1,20 @@
+/*
+2024-01-24 - JRK
+Swiss Army SQL Query for Joplin
+*/
+/*pandas*
+timezone = "America/Chicago"
+[parse_dates]
+updated_date = {format = "%Y-%m-%d %H:%M:%S", errors = "coerce", exact = false}
+created_date = {format = "%Y-%m-%d %H:%M:%S", errors = "coerce", exact = false}
+[dtype]
+joplin_id = "string"
+parent_id = "string"
+title = "string"
+body = "string"
+source = "string"
+source_application = "string"
+*pandas*/
 select
     id as joplin_id,
     parent_id,
