ba4138bb55e2abedff9706eae156668f33ccf802
Justin <kellerjustin@protonmail.com>
2024-09-11 13:47:02 -0500

    comments

--- sql/payments.sql
+++ sql/payments.sql
@@ -1,3 +1,12 @@
+/*
+ ~ 2024 = initial creation of SQL
+   The following SQL will pull all payment splits in the database.
+   This is not terribly useful by itself and is intended to be joined
+   with an invoice dataframe to capture whether or not an invoice was paid
+
+   The key to joining the tables is the lot_guid field in the invoices table
+ */
+
 select splits.value_num / cast(splits.value_denom AS DOUBLE PRECISION) as payment_amt,
     splits.lot_guid
 from splits
